aec85fd0cb36909b190914efe6b06155fed6dcbf
bgblackley1 <bgblackley1@outlook.com>
2021-12-02 14:43:00 +0000

    Aidan

--- src/Cheat.py
+++ src/Cheat.py
@@ -10,12 +10,22 @@ class Cheat:
         self.last_played = []
         self.turn = 0
         self.players = self.get_players()
-        self.deck = self.deal_deck()
+        self.deck = self.deal_deck(self.players)
         winner = None
+        while winner == None:
+            print(self.print_cards(self.deck, self.turn%self.players))
+            cards = self.input_playcards()
+            self.deck = self.remove_cards(self.deck, self.turn % self.players, cards)
+            self.add_to_pot(cards)
+            cheatcards = self.input_cheat_cards()
+            self.is_cheat(cheatcards)
+            winner = self.find_winner(self.deck)
+            self.turn += 1
+
 
 
     def get_players(self):
-        players = input('Number of player')
+        players = int(input('Number of player'))
         return (players)
 
     def deal_deck(self, players):
@@ -35,27 +45,40 @@ class Cheat:
     def input_playcards(self):
         card = input('What card(s) would you like to play: ')
         cards = []
-        cards.append(card)
+        cards.append(card.upper())
         while card != '':
             card = input('What card(s) would you like to play: ')
-            cards.append(card)
+            cards.append(card.upper())
+        valid = self.validate_entry(self.deck, self.turn%self.players, cards)
+        if valid == False:
+            print('The cards must be in your hand')
+            card = input('What card(s) would you like to play: ')
+            cards = []
+            cards.append(card.upper())
+            while card != '':
+                card = input('What card(s) would you like to play: ')
+                cards.append(card.upper())
+            valid = self.validate_entry(self.deck, self.turn % self.players, cards)
         return cards
 
     def validate_entry(self, deck, player, cards):
         valid = True
         for card in cards:
             if card not in deck[player]:
-                valid = False
+                if card != '':
+                    valid = False
         return valid
 
     def remove_cards(self, deck, player, cards):
         for card in cards:
-            deck[player].remove(card)
+            if card != '':
+                deck[player].remove(card)
         return deck
 
     def add_to_pot(self, cards):
-        self.pot.append(cards)
-        self.last_played = cards
+        for i in range(0, len(cards)-1):
+            self.pot.append(cards[i])
+        self.last_played = cards[:-1]
 
     def input_cheat_cards(self):
         for i in range(50):
@@ -68,31 +91,37 @@ class Cheat:
             cards.append(card)
         return cards
 
-    def is_cheat(self):
+    def is_cheat(self, cheatcards):
         cheat = input('Do you think it is cheat(Y/N): ')
         # asks first person for player number
-        player = input('What player are you: ')
         if cheat == 'Y':
-            is_cheat = self.validate_is_cheat(self.input_cheat_cards(), self.last_played)
+            player = int(input('What player are you: '))
+            is_cheat = self.validate_is_cheat(cheatcards, self.last_played)
             if is_cheat == False:
                 self.deck[player] = self.add_cards(self.deck[player], self.pot)
             else:
                 self.deck[self.turn % self.players] = self.add_cards(self.deck[self.turn % self.players], self.pot)
+                self.turn = player - 1
 
     def validate_is_cheat(self, cards, last_played):
         is_cheat = False
         for card in last_played:
             if card[1:] in cards:
-                cards.remove(card[1])
+                cards.remove(card[1:])
             else:
                 is_cheat = True
         return is_cheat
 
     def add_cards(self, hand, pot):
-        pot += hand
-        return pot
+        for i in range(0, len(pot)):
+            hand.append(pot[i])
+        return hand
 
-    def find_winner(self,hands):
+    def find_winner(self, hands):
         for i in range(len(hands)):
             if not hands[i]:
                 return i
+
+game = Cheat()
+if __name__ == '__main__':
+    game.Main()
