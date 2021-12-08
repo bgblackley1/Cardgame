7cd6822fdfa17377530159a3378e9e163083443a
bgblackley1 <bgblackley1@outlook.com>
2021-12-08 14:39:08 +0000

    Aidan

--- src/Cheat.py
+++ src/Cheat.py
@@ -1,32 +1,51 @@
 from src.PlayingCard import PlayingCard
+from Computer import Computer_Player
 import os
 
 
 class Cheat:
     Card = PlayingCard()
 
-    def Main(self):
-        self.pot = []
-        self.last_played = []
-        self.turn = 0
+    def __init__(self):
         self.players = self.get_players()
+        self.computers = self.set_computer_players()
+        self.turn = 0
+        self.last_played = []
+        self.pot = []
+        self.computer_players = [None] * (self.players - self.computers)
+        self.computer_or_player = ['p'] * (self.players - self.computers)
+        self.Main()
+
+    def Main(self):
         self.deck = self.deal_deck(self.players)
+        for i in range(self.computers):
+            self.computer_or_player.append('c')
+            self.computer_players.append(Computer_Player(self.deck[self.players - self.computers + i]))
         winner = None
         while winner == None:
-            print(self.print_cards(self.deck, self.turn%self.players))
-            cards = self.input_playcards()
-            self.deck = self.remove_cards(self.deck, self.turn % self.players, cards)
-            self.add_to_pot(cards)
-            cheatcards = self.input_cheat_cards()
-            self.is_cheat(cheatcards)
+            if self.computer_or_player[self.turn%self.players] == 'p':
+                print(self.print_cards(self.deck, self.turn%self.players))
+                cards = self.input_playcards()
+                self.deck = self.remove_cards(self.deck, self.turn % self.players, cards)
+                self.add_to_pot(cards)
+                cheatcards = self.input_cheat_cards()
+                self.is_cheat(cheatcards)
+            else:
+                comp_cards = self.computer_players[self.turn%self.players].play_card(self.last_played)
+                self.deck = self.remove_cards(self.deck, self.turn % self.players, comp_cards[0])
+                print('The computer played: ', ', '.join(comp_cards[1]))
+                self.add_to_pot(comp_cards[0])
+                self.is_cheat(comp_cards[1])
             winner = self.find_winner(self.deck)
             self.turn += 1
 
-
-
     def get_players(self):
-        players = int(input('Number of player'))
+        players = int(input('Number of players: '))
         return (players)
+
+    def set_computer_players(self):
+        computers = int(input('Number of computers: '))
+        return computers
 
     def deal_deck(self, players):
         self.deck = self.Card.generate_deck()
@@ -102,6 +121,14 @@ class Cheat:
             else:
                 self.deck[self.turn % self.players] = self.add_cards(self.deck[self.turn % self.players], self.pot)
                 self.turn = player - 1
+        else:
+            for i in range((self.players - self.computers), len(self.computer_players)):
+                cheat = self.computer_players[i].check_is_cheat(cheatcards)
+                if cheat == True:
+                    self.deck[self.turn % self.players] = self.add_cards(self.deck[self.turn % self.players], self.pot)
+                    print('the computer called cheat')
+                    self.turn = self.players + i - 1
+
 
     def validate_is_cheat(self, cards, last_played):
         is_cheat = False
@@ -123,5 +150,3 @@ class Cheat:
                 return i
 
 game = Cheat()
-if __name__ == '__main__':
-    game.Main()
--- src/Computer.py
+++ src/Computer.py
@@ -3,25 +3,70 @@ import random
 from src.PlayingCard import PlayingCard
 
 
-class Computer:
+class Computer_Player:
 
     def __init__(self, hand):
         self.hand = hand
-        self.playingcard = PlayingCard
+        self.playingcard = PlayingCard()
+        self.above, self.below, self.equal = [0], [0], [0]
 
     def play_card(self, last_cards_played):
-        self.playingcard.convert_faces_to_numbers(last_cards_played)
+        self.above, self.below, self.equal = [0], [0], [0]
+        self.last_cards_played = last_cards_played
+        self.playingcard.convert_faces_to_numbers(self.last_cards_played)
         self.playingcard.convert_faces_to_numbers(self.hand)
-        above, below, equal = 0
         for card in self.hand:
-            if int(card[1:]) == int(last_cards_played[0]) - 1:
-                below += 1
-            elif int(card[1:]) == int(last_cards_played[0]):
-                below += 1
-            elif int(card[1:]) == int(last_cards_played[0]) + 1:
-                below += 1
+            if int(card[1:]) == int(last_cards_played[0][1:]) - 1:
+                self.below[0] += 1
+                self.below.append(card)
+            elif int(card[1:]) == int(last_cards_played[0][1:]):
+                self.equal[0] += 1
+                self.equal.append(card)
+            elif int(card[1:]) == int(last_cards_played[0][1:]) + 1:
+                self.above[0] += 1
+                self.above.append(card)
+        return(self.check_can_play())
 
-        return actual, cheat
+    def check_can_play(self):
+        cards = [self.below[0], self.equal[0], self.above[0]]
+        cards.sort()
+        if cards[2] != 0:
+            if cards[2] == self.below[0]:
+                played = self.below[1:]
+                said = [self.below[1][1:]] * self.below[0]
+                cardsreturn = [played, said]
+                return cardsreturn
+            elif cards[2] == self.equal[0]:
+                played = self.below[1:]
+                said = [self.below[1][1:]] * self.below[0]
+                cardsreturn = [played, said]
+                return cardsreturn
+            elif cards[2] == self.above[0]:
+                played = self.below[1:]
+                said = [self.below[1][1:]] * self.below[0]
+                cardsreturn = [played, said]
+                return cardsreturn
+        else:
+            self.must_cheat()
 
-    def check_is_cheat(self, cards_played):
-        pass
+    def must_cheat(self):
+        if len(self.hand) >= 4:
+            playing = random.randint(1, 4)
+        else:
+            playing = random.randint(1, len(self.hand))
+        value = random.randint(-1, 1)
+        cards = []
+        for i in range(playing):
+            cards.append(self.hand[random.randint(0, len(self.hand) - 1)])
+        cards_said = [self.last_cards_played[0] + value] * playing
+        return cards, cards_said
+
+    def check_is_cheat(self, last_cards_played):
+        num_cards = 0
+        for card in self.hand:
+            if (card[1:]) == (last_cards_played[0]):
+                num_cards += 1
+        if num_cards + len(last_cards_played) - 1 > 4:
+            return True
+        else:
+            return False
